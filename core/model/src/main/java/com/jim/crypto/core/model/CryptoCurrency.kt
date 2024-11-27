e7702e44f9657c0b887bbb70a96da55d9adbecc6
Jim Wong <yfwong04@gmail.com>
2024-11-27 21:09:31 +1030

    implement :core:model module

---
+++ core/model/src/main/java/com/jim/crypto/core/model/CryptoCurrency.kt
@@ -0,0 +1,7 @@
+package com.jim.crypto.core.model
+
+data class CryptoCurrency(
+  val id: String,
+  val name: String,
+  val symbol: String
+)
---
+++ core/model/src/main/java/com/jim/crypto/core/model/FiatCurrency.kt
@@ -0,0 +1,8 @@
+package com.jim.crypto.core.model
+
+data class FiatCurrency(
+  val id: String,
+  val name: String,
+  val symbol: String,
+  val code: String
+)
